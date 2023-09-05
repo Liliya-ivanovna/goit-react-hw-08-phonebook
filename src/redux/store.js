6ad3f37f97401c6595caf25e7bba3d206bf9b6e0
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-09-05 10:33:00 +0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './phonebook/filterSlice';
 import { contactsReducer } from './phonebook/phonebookSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
 
-const authPersistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
-};
+// const authPersistConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
 
 export const store = configureStore({
   reducer: {
@@ -26,13 +26,13 @@ export const store = configureStore({
     filter: filterReducer,
     //auth: persistReducer(authPersistConfig, authReducer),
   },
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  },
+  // middleware(getDefaultMiddleware) {
+  //   return getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   });
+  // },
 });
 
-export const persistor = persistStore(store);
+//export const persistor = persistStore(store);
